6554286a38eabfa24b6612c3bfe065d9383bf115
sreesankar k <143923345+lonecoder7@users.noreply.github.com>
2026-09-19 19:47:32 +0530

    Update logo import path in Wordmark component

--- src/components/site/Wordmark.tsx
+++ src/components/site/Wordmark.tsx
@@ -1,6 +1,6 @@
 import { Link } from "@tanstack/react-router";
 import { cn } from "@/lib/utils";
-import logo from "@/assets/wellcore-logo.png.asset.json";
+import logo from "@/assets/wellcore-logo.png";
 
 /**
  * Official Wellcore Health Solutions logo lockup.
